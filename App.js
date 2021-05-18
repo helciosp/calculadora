d39e7a69c88702caa521866ef3940a5ad17575d5
helciosp43 <helciosp43@gmail.com>
2021-05-18 11:26:49 -0300

    Duas importações do mesmos lugar

--- App.js
+++ App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import {useState} from 'react'
+import React, {useState} from 'react';
 import { StyleSheet, Text, View, TouchableOpacity, Button } from 'react-native';
 
 export default function App() {
